efe85be2eb2a443f7b0585027372de8fb04f359f
Leon <info@leon.vankammen.eu>
2021-02-17 17:35:44 +0100

    update documentation

--- README.md
+++ README.md
@@ -30,6 +30,26 @@ const Parse = require('parse/node');
 const ParseQ = require('./ParseQ')(Parse,Q)
 const client = new mongodb.MongoClient(url, { useNewUrlParser: true,useUnifiedTopology:true })
 
+let work = (queue) => {
+  done = () => work(queue) // repeat infinitely!
+  queue.get((err, msg) => {
+	try{
+		if( !msg )  return done()
+		if( err  )  throw err 
+		console.dir(msg)
+		console.log('msg.id=' + msg.id)
+		console.log('msg.ack=' + msg.ack)
+		console.log('msg.payload=' + msg.payload) // 'Hello, World!'
+		console.log('msg.tries=' + msg.tries)
+		queue.ack( msg.ack, console.error )
+		done()
+	}catch(e){ 
+		console.error(e)
+		done()
+	}
+  }) 
+}
+
 client.connect( async (err) => {
   const db = client.db('test')
   
@@ -40,13 +60,8 @@ client.connect( async (err) => {
   	// 'id' is returned, useful for logging.
   })
 
-  queue.get((err, msg) => {
-  	console.log('msg.id=' + msg.id)
-   	console.log('msg.ack=' + msg.ack)
-   	console.log('msg.payload=' + msg.payload) // 'Hello, World!'
-   	console.log('msg.tries=' + msg.tries)
-  }) 
- 
+  work(queue)
+
 })
 ```
 
--- index.js
+++ index.js
@@ -9,23 +9,38 @@ const client = new mongodb.MongoClient(url, { useNewUrlParser: true,useUnifiedTo
 Parse.initialize( process.env.APP_ID, process.env.API_KEY_JAVASCRIPT, process.env.MASTER_KEY )
 Parse.serverURL = process.env.SERVER_URL
 
+let work = (queue) => {
+  done = () => work(queue) // repeat infinitely!
+  queue.get((err, msg) => {
+	try{
+		if( !msg )  return done()
+		if( err  )  throw err 
+		console.dir(msg)
+		console.log('msg.id=' + msg.id)
+		console.log('msg.ack=' + msg.ack)
+		console.log('msg.payload=' + msg.payload) // 'Hello, World!'
+		console.log('msg.tries=' + msg.tries)
+		queue.ack( msg.ack, console.error )
+		done()
+	}catch(e){ 
+		console.error(e)
+		done()
+	}
+  }) 
+}
+
 client.connect( async (err) => {
   const db = client.db('test')
   console.log("connected")
   try{
     const queue = await ParseQ.get(db,'Q_email')
     
-      queue.add('Hello, World!', (err, id) => {
+      queue.add({foo:123}, (err, id) => {
       	// Message with payload 'Hello, World!' added.
       	// 'id' is returned, useful for logging.
       })
 
-      queue.get((err, msg) => {
-      	console.log('msg.id=' + msg.id)
-      	console.log('msg.ack=' + msg.ack)
-      	console.log('msg.payload=' + msg.payload) // 'Hello, World!'
-      	console.log('msg.tries=' + msg.tries)
-      }) 
+	  work(queue)
     
   }catch(e){ console.error(e) }
  
